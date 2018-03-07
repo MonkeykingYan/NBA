57a8edbfa04bb1cbc5081e53e1d4b8dd1ed1dfbc
YanMa <monkeykingyan@gmail.com>
2018-03-07 15:42:49 -0500

    kmeans

--- TeamClustering.py
+++ TeamClustering.py
@@ -3,28 +3,59 @@ from pyspark.ml.feature import StandardScaler
 from pyspark.ml.linalg import Vectors
 from pyspark.sql.functions import col
 
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.clustering import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+from pyspark.sql import SQLContext
 
+# ReadFile
+FEATURES_COL = ['Pts', 'Opp Pts', 'Pace', 'Off Eff', 'Def Eff']
+path = 'data/17_18_advanced.csv'
 spark = SparkSession.builder.appName('NBA-Analysis').getOrCreate()
-data = spark.read.csv('data/17_18_advanced.csv', header=True, inferSchema=True)
+data = spark.read.csv(path, header=True, inferSchema=True)
+data.printSchema()
 
-data.show()
-cols = ['Pts', 'Avg Mrg','EFg%','Ts%']
-assembler = VectorAssembler(inputCols=cols, outputCol='features')
-assembled_data = assembler.transform(data)
+vecAssembler = VectorAssembler(inputCols=FEATURES_COL, outputCol="features")
+df_kmeans = vecAssembler.transform(data).select('Team', 'features')
+df_kmeans.show()
 
-scaler = StandardScaler(inputCol='features', outputCol='scaledFeatures')
-scaler_model = scaler.fit(assembled_data)
-scaled_data = scaler_model.transform(assembled_data)
-scaled_data.printSchema()
-scaled_data.select('scaledFeatures').show()
+cost = np.zeros(20)
+for k in range(2, 20):
+    kmeans = KMeans().setK(k).setSeed(1).setFeaturesCol("features")
+    model = kmeans.fit(df_kmeans.sample(False, 0.1, seed=42))
+    cost[k] = model.computeCost(df_kmeans)
 
-k_means_2 = KMeans(featuresCol='scaledFeatures', k=2)
-k_means_3 = KMeans(featuresCol='scaledFeatures', k=3)
-model_k2 = k_means_2.fit(scaled_data)
-model_k3 = k_means_3.fit(scaled_data)
+plt.interactive(True)
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+ax.plot(range(2, 20), cost[2:20])
+ax.set_xlabel('k')
+ax.set_ylabel('cost')
+fig.show()
+plt.savefig('K_Selection.png')
 
-model_k3_data = model_k3.transform(scaled_data)
-model_k3_data.groupBy('prediction').count()
-model_k3_data.sort(col("prediction")).show()
+k = 4
+kmeans = KMeans().setK(k).setSeed(1).setFeaturesCol("features")
+model = kmeans.fit(df_kmeans)
+centers = model.clusterCenters()
+
+print("Cluster Centers: ")
+for center in centers:
+    print(center)
+
+transformed = model.transform(df_kmeans).select('Team', 'prediction')
+rows = transformed.collect()
+print(rows[:3])
+
+df_pred = data.join(transformed, 'Team')
+pddf_pred = df_pred.toPandas().set_index('Team')
+
+threedee = plt.figure(figsize=(12,10)).gca(projection='3d')
+threedee.scatter(pddf_pred['Pts'], pddf_pred['Opp Pts'], pddf_pred['Pace'], c=pddf_pred.prediction)
+threedee.set_xlabel('x')
+threedee.set_ylabel('y')
+threedee.set_zlabel('z')
+plt.show()
+plt.savefig('KMeans.png')
