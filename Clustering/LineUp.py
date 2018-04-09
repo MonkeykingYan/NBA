2a4e2b9319d3c89115aea466e9d86c9b03384cbd
YanMa <monkeykingyan@gmail.com>
2018-04-09 13:25:30 -0400

    lineup

--- Clustering/LineUp.py
+++ Clustering/LineUp.py
@@ -9,38 +9,6 @@ spark = SparkSession.builder.appName('NBA-Analysis').getOrCreate()
 df = pd.read_csv('/Users/mayan/Desktop/BigData/project/Project_BigData/data/lineups_1516.csv')
 df_players = pd.read_csv('playersClusters.csv')
 
-playersSchema = StructType([StructField("player", StringType(), True) \
-                               , StructField("team_id", StringType(), True) \
-                               , StructField("yr", IntegerType(), True) \
-                               , StructField("prediction", IntegerType(), True)])
-mySchema = StructType([StructField("RK", IntegerType(), True) \
-                          , StructField("Lineup", StringType(), True) \
-                          , StructField("TM", StringType(), True) \
-                          , StructField("Season", StringType(), True) \
-                          , StructField("G", IntegerType(), True) \
-                          , StructField("MP", DoubleType(), True) \
-                          , StructField("PTS", DoubleType(), True) \
-                          , StructField("PG", DoubleType(), True) \
-                          , StructField("PGA", DoubleType(), True) \
-                          , StructField("PG%", DoubleType(), True) \
-                          , StructField("3P", DoubleType(), True) \
-                          , StructField("3PA", DoubleType(), True) \
-                          , StructField("3P%", DoubleType(), True) \
-                          , StructField("eFG%", DoubleType(), True) \
-                          , StructField("FT", DoubleType(), True) \
-                          , StructField("FTA", DoubleType(), True) \
-                          , StructField("FT%", DoubleType(), True) \
-                          , StructField("ORB", DoubleType(), True) \
-                          , StructField("ORB%", DoubleType(), True) \
-                          , StructField("DRB", DoubleType(), True) \
-                          , StructField("DRB%", DoubleType(), True) \
-                          , StructField("TRB", DoubleType(), True) \
-                          , StructField("TRB%", DoubleType(), True) \
-                          , StructField("AST", DoubleType(), True) \
-                          , StructField("STL", DoubleType(), True) \
-                          , StructField("BLK", DoubleType(), True) \
-                          , StructField("TOV", DoubleType(), True) \
-                          , StructField("PF", DoubleType(), True)])
 # mySchema = StructType([StructField("RK", IntegerType(), True) \
 #                           , StructField("Lineup", StringType(), True) \
 #                           , StructField("TM", StringType(), True) \
@@ -75,14 +43,16 @@ df_p = spark.createDataFrame(df_players)
 df_p.show(df_p.count(), False)
 
 # df = spark.createDataFrame(df, schema=mySchema)
-df = spark.createDataFrame(df)
-df.show()
+df_lineup = spark.createDataFrame(df)
+df_lineup.show()
 
 
 def getPlayers(df, teamName, k):
     players = {}
     i = 0
-    for it in df.select('Lineup').where(col('Tm') == teamName).collect():
+    for it in df.select('Lineup','MP').where(col('Tm') == teamName).collect():
+        print(it[0])
+        print(it[1])
         it = it[0].split('|')
         players[i] = it
         if (i == k):
@@ -96,13 +66,15 @@ def getPlayers(df, teamName, k):
 
 def getTeamFeaturs(teamName, rank):
     feature = []
-    res = getPlayers(df, teamName, rank)
+    res = getPlayers(df_lineup, teamName, rank)
     for i in res.keys():
         players = res[i]
         for p in players:
             p = p.strip()
-            f = df_p.filter(df_p.player.like('%' + p[4:len(p) - 1] + '%')).filter(
-                df_p.player.like('%' + p[0] + '%')).distinct()
+            print('%' + p[3:len(p)] + '%')
+            print('%,' + p[0] + '%')
+            f = df_p.filter(df_p.player.like('%' + p[3:len(p)] + '%')).filter(
+                df_p.player.like('%,' + p[0] + '%')).distinct()
             feac = f.select('prediction').collect()
             if (len(feac) != 0):
                 feature.append(feac[0][0])
