f7e0814f84fcef7dccb3dbddd11796fbb741d9c9
YanMa <monkeykingyan@gmail.com>
2018-04-09 22:41:29 -0400

    kmeans

--- Clustering/testPCA.py
+++ Clustering/testPCA.py
@@ -21,36 +21,37 @@ AllTeams = ['OKC', 'GSW', 'SAS', 'CLE', 'LAC',
             'MIL', 'PHO', 'CHI', 'MIN', 'DEN']
 
 TeamDic = {}
+words = []
 for team in AllTeams:
     lis = data.select(team).collect()
     TeamDic[team] = lis[0][0]
-    print(lis[0][0])
-    print(type(lis[0][0]))
+    words.append(lis[0][0])
+print(TeamDic)
 
 # words = ['apple', 'Doppler', 'applaud', 'append', 'barker',
 #          'baker', 'bismark', 'park', 'stake', 'steak', 'teak', 'sleek']
-#
-# dist = [distance.edit_distance(words[i], words[j])
-#         for i in range(1, len(words))
-#         for j in range(0, i)]
-#
-# labels, error, nfound = PC.kmedoids(dist, nclusters=3)#kmedoids(dist, nclusters=3)
-# cluster = dict()
-# for word, label in zip(words, labels):
-#     cluster.setdefault(label, []).append(word)
-# for label, grp in cluster.items():
-#     print(grp)
-#
-# data = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
-# data = np.array(data)
-# np.savetxt("foo.csv", data, delimiter=",")
-#
-#
-# schema = StructType([StructField('Name', StringType(), True),
-#                      StructField('DateTime', TimestampType(), True),
-#                      StructField('Age', IntegerType(), True)])
-# print(data)
-# pca = PCA(n_components=3)
-# pca.fit(data)
-# X = pca.transform(data)
-# print(X)
+
+dist = [distance.edit_distance(words[i], words[j])
+        for i in range(1, len(words))
+        for j in range(0, i)]
+
+labels, error, nfound = PC.kmedoids(dist, nclusters=3)#kmedoids(dist, nclusters=3)
+cluster = dict()
+for word, label in zip(words, labels):
+    cluster.setdefault(label, []).append(word)
+for label, grp in cluster.items():
+    print(grp)
+
+data = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+data = np.array(data)
+np.savetxt("foo.csv", data, delimiter=",")
+
+
+schema = StructType([StructField('Name', StringType(), True),
+                     StructField('DateTime', TimestampType(), True),
+                     StructField('Age', IntegerType(), True)])
+print(data)
+pca = PCA(n_components=3)
+pca.fit(data)
+X = pca.transform(data)
+print(X)
